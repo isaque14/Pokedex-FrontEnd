6df0cbd6d54906adca7506499e263fe12ce7de8f
isaque14 <isaquediniz14@gmail.com>
2023-06-10 12:02:44 -0300

    feat(pokedexapiservice): pokedexApiService é o serciço de consumo do BackEnd

---
+++ src/app/services/pokedex-api.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokedexApiService {
+
+  private url: string = 'https://localhost:7130/api/pokemon'
+
+  constructor(private http: HttpClient) { }
+
+  get apiListAllPokemons():Observable<any>{
+    return this.http.get<any>(this.url).pipe(
+      tap(res => res),
+      tap(res => {
+        console.log(res)
+      })
+    )
+  }
+}
--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PokedexApiService } from 'src/app/services/pokedex-api.service';
 
 @Component({
   selector: 'poke-list',
   templateUrl: './poke-list.component.html',
   styleUrls: ['./poke-list.component.scss']
 })
-export class PokeListComponent {
+export class PokeListComponent implements OnInit{
+  constructor(private pokedexApiService: PokedexApiService){}
 
+  ngOnInit(): void {
+    this.pokedexApiService.apiListAllPokemons.subscribe(
+      res => res
+    );
+  }
 }
