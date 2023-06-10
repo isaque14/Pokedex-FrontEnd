59b2ac77a4be371e4c70552752ac55197515d729
isaque14 <isaquediniz14@gmail.com>
2023-06-09 22:09:18 -0300

    feat(headercomponent): componente de cabeçalho

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { RoutingModule } from './routing.module';
 import { DetailsComponent } from './details/details.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
@@ -10,9 +11,11 @@ import { DetailsComponent } from './details/details.component';
     HomeComponent,
     DetailsComponent
   ],
+
   imports: [
     CommonModule,
-    RoutingModule
+    RoutingModule,
+    SharedModule
   ]
 })
 export class PagesModule { }
