e37503c03eb9cb79f32a7ffb55b24d26749d6882
isaque14 <isaquediniz14@gmail.com>
2023-06-10 15:14:11 -0300

    feat(poke-list-components): listagem de Pokemons com dados do BackEnd API

--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -6,12 +6,50 @@ import { PokedexApiService } from 'src/app/services/pokedex-api.service';
   templateUrl: './poke-list.component.html',
   styleUrls: ['./poke-list.component.scss']
 })
-export class PokeListComponent implements OnInit{
-  constructor(private pokedexApiService: PokedexApiService){}
+export class PokeListComponent implements OnInit {
+  constructor(private pokedexApiService: PokedexApiService) { }
+
+  public getAllPokemons: any;
 
   ngOnInit(): void {
     this.pokedexApiService.apiListAllPokemons.subscribe(
-      res => console.log(res)
+      res => {
+        this.getAllPokemons = res.object;
+        this.convertTypeNumbersToNames();
+        console.log(this.getAllPokemons);
+      }
     );
+  }
+
+  private convertTypeNumbersToNames(): void {
+    const typeMappings = [
+      'Normal',
+      'Fire',
+      'Water',
+      'Electric',
+      'Grass',
+      'Ice',
+      'Fighting',
+      'Poison',
+      'Ground',
+      'Flying',
+      'Psychic',
+      'Bug',
+      'Rock',
+      'Ghost',
+      'Dragon',
+      'Dark',
+      'Steel',
+      'Fairy',
+      'Unknown',
+      'Shadow'
+    ];
+
+    this.getAllPokemons.forEach((pokemon: any) => {
+      const convertedTypes = pokemon.type.map((typeNumber: number) => {
+        return typeMappings[typeNumber];
+      });
+      pokemon.type = convertedTypes;
+    });
   }
 }
